2863ba04c0076abf1c24f0276194df4d31f03d1d
awawdasdad <ferminzin04@gmail.com>
2025-03-02 18:26:00 +0100

    Clase car

--- src/main/java/com/projectdws/alquilercoches/classes/Car.java
+++ src/main/java/com/projectdws/alquilercoches/classes/Car.java
@@ -1,3 +1,67 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class car {
-    
+    private String name_car;
+    private int licensePlate;
+    private int kmsPerHour;
+    private int year;
+    private int price;
+    private List<comment> comments = new ArrayList<>();
+
+    public car(String name_car, int licensePlate, int kmsPerHour, int year, int price) {
+        this.name_car = name_car;
+        this.licensePlate = licensePlate;
+        this.kmsPerHour = kmsPerHour;
+        this.year = year;
+        this.price = price;
+    }
+
+    public String getName_car() {
+        return name_car;
+    }
+
+    public void setName_car(String name_car) {
+        this.name_car = name_car;
+    }
+
+    public int getLicensePlate() {
+        return licensePlate;
+    }
+
+    public void setLicensePlate(int licensePlate) {
+        this.licensePlate = licensePlate;
+    }
+
+    public int getKmsPerHour() {
+        return kmsPerHour;
+    }
+
+    public void setKmsPerHour(int kmsPerHour) {
+        this.kmsPerHour = kmsPerHour;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public List<comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<comment> comments) {
+        this.comments = comments;
+    }
 }
